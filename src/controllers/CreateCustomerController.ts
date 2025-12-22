6ed68159179f867bc8b32814681df9836f4a491a
lucas camilo vonlabs <lucas.camilo@vonbraunlabs.com.br>
2025-12-22 18:55:08 -0300

    iniciei o desenvolviemnto de rotas testes

---
+++ src/controllers/CreateCustomerController.ts
@@ -0,0 +1,14 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { CreateCustomerService } from "../services/CreateCustomerService";
+
+class CreateCustomerController {
+    async handle (request: FastifyRequest, reply: FastifyReply) {
+        const customerService = new  CreateCustomerService();
+
+        const customer = await customerService.execute();
+
+        reply.send(customer);
+    }
+}
+
+export { CreateCustomerController };
---
+++ src/services/CreateCustomerService.ts
@@ -0,0 +1,11 @@
+import prismaClient from "../prisma";
+
+class CreateCustomerService {
+    async execute() {
+        console.log("Rota foi chamada");
+        
+        return { ok: true}
+    }
+}
+
+export { CreateCustomerService };
